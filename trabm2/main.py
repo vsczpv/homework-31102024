b153d9c068d098fc53ba6e509285b9d03039c52f
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-10-31 11:26:54 -0300

    Add SEEDS code and test results
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- trabm2/main.py
+++ trabm2/main.py
@@ -43,6 +43,37 @@ def threshes(img_orig, img_dsalt, img_dgaus):
         alt = treat_fingerprint(nrm)
         cv2.imwrite('results/dig_treat_gbasic_' + str(i) + '.png', alt)
 
+# https://github.com/opencv/opencv_contrib/blob/4.x/samples/python2/seeds.py
+def seeds(img_or, title, nsp, prir, levels, iter):
+
+    img_in = cv2.cvtColor(img_or, cv2.COLOR_BGR2HSV)
+    height, width, channels = img_in.shape
+
+    num_superpixels = nsp # 6
+    prior = prir # 4
+    num_levels = levels # 30
+    num_histogram_bins = 10
+    seeds = cv2.ximgproc.createSuperpixelSEEDS(width, height, channels, num_superpixels, num_levels, prior, num_histogram_bins)
+    color_img    = np.zeros((height,width,3), np.uint8)        
+    color_img[:] = (0, 0, 255)
+
+    seeds.iterate(img_in, iter)
+
+    labels = seeds.getLabels()
+
+    # labels output: use the last x bits to determine the color
+    num_label_bits = 2
+    labels &= (1<<num_label_bits)-1
+    labels *= 1<<(16-num_label_bits)
+
+    mask = seeds.getLabelContourMask(False)
+
+    mask_inv = cv2.bitwise_not(mask)
+    result_bg = cv2.bitwise_and(img_or, img_or, mask=mask_inv)
+    result_fg = cv2.bitwise_and(color_img, color_img, mask=mask)
+    result = cv2.add(result_bg, result_fg)
+
+    cv2.imwrite(title, result)
 
 def main():
     img_orig  = util.openimg('data/digital.jpg')
@@ -67,3 +98,12 @@ def main():
             kmeans.KMeans3D(util.openimg(path, color=True), k=j, max_iterations=30,  title=str(i))
             kmeans.KMeans3D(util.openimg(path, color=True), k=j, max_iterations=100, title=str(i))
     
+    seeds(util.openimg('data/face1.jpg', color=True), 'results/seeds1.png', nsp=6, prir=4, levels=30, iter=2000)
+    seeds(util.openimg('data/face2.jpg', color=True), 'results/seeds2.png', nsp=2, prir=3, levels=30, iter=2000)
+    seeds(util.openimg('data/face2.jpg', color=True), 'results/hi_seeds2.png', nsp=200, prir=3, levels=30, iter=2000)
+    seeds(util.openimg('data/face2.jpg', color=True), 'results/me_seeds2.png', nsp=20, prir=3, levels=30, iter=2000)
+    seeds(util.openimg('data/face2.jpg', color=True), 'results/15_seeds2.png', nsp=15, prir=3, levels=30, iter=2000)
+    seeds(util.openimg('data/face2.jpg', color=True), 'results/10_seeds2.png', nsp=10, prir=3, levels=30, iter=2000)
+    seeds(util.openimg('data/face3.jpg', color=True), 'results/seeds3.png', nsp=3, prir=5, levels=30, iter=2000)
+    seeds(util.openimg('data/face4.jpg', color=True), 'results/seeds4.png', nsp=5, prir=2, levels=30, iter=2000)
+    seeds(util.openimg('data/face4.jpg', color=True), 'results/hi_seeds4.png', nsp=2000, prir=5, levels=30, iter=2000)
